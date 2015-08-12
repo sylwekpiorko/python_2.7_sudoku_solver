bbce2c8bc59d71a96a4b96b9fd6bcfc78d75cf45
sylwekpiorko <sylwesterpiorkowski@yahoo.ie>
2015-08-13 00:28:53 +0100

    Update sudoku_solver_v1.py

--- sudoku_solver_v1.py
+++ sudoku_solver_v1.py
@@ -2,7 +2,19 @@
 sudoku = "..5..79...2..9..4.3..8....66.....4...7.....3...8.....28....2..3.6..7..9...45....."
 
 def sudoku_grid(data):
-    print data
+    for i in range(len(data)+1):
+        print data[i-1],
+        if i % 27 == 0: 
+            print "\n- - - + - - - + - - -"
+            
+        elif i % 9== 0:
+            print "\n"
+            
+        elif i % 3 == 0:
+            print "|",
+        
+        
+    return "Done"
     
-sudoku_grid(sudoku)
+print sudoku_grid(sudoku)
 
