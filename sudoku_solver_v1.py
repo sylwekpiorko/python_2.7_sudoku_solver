deb19276fa729b983e938326b861f15f4c9a8524
sylwekpiorko <sylwesterpiorkowski@yahoo.ie>
2015-08-13 03:31:23 +0100

    Update sudoku_solver_v1.py

--- sudoku_solver_v1.py
+++ sudoku_solver_v1.py
@@ -1,19 +1,19 @@
 # input data
 sudoku = "..5..79...2..9..4.3..8....66.....4...7.....3...8.....28....2..3.6..7..9...45....."
 
-print len(sudoku)
+print "Number of sudoku elements: %d" % len(sudoku)
 
 def sudoku_grid(data):
     for i in range(len(data)):
         
         #print "%d>" % i,
-        print data[i-1],    
+        print data[i], 
         
-        if i % 9== 0 and i > 0:
+        if (i+1) % 9== 0 and i > 0:
             print "\n"
-        if i % 3 == 0 and i >0 and not i % 9 == 0:
+        if (i+1) % 3 == 0 and i >0 and not (i+1) % 9 == 0:
             print "|",
-        if i % 27 == 0 and i > 0: 
+        if (i+1) % 27 == 0 and i > 0 and i < 80: 
             print "- - - + - - - + - - -"
             
     return "Done"
